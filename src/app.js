020311074ce251653511289a0807aa21b08f42c8
EmoryC <emory-crane@live.nmit.ac.nz>
2022-05-01 20:04:13 +1200

    notations for start

--- src/app.js
+++ src/app.js
@@ -1,19 +1,32 @@
-class _Date{
+//SECTION Requirement #1
+//  Creating a const for the names of the months
+const _Months = ['','Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec']
+//Creating a const for the days within each month
+const dayspermonth = [0,31,28,31,30,31,30,31,31,30,31,30,31]
+//Both have been left with whitespaces of null values in front so that each month correctly
+//corresponds to it's calander number
+
+//Creating a class _Date with which I will construct all functions (methods)
+class _Date{ 
     constructor(day,month,year){
-      if(year>2030){
+      if(year>2030){ //Setting an arbirary limit to the years to try it
         console.log("Invalid Input: Maximum year 2030")
     } else if(year<2000){
         console.log("Invalid Input: Minumim year 2000")
     } else {
         this.day = day
         this.month = month
-        this.year = year
-    }
+        this.year = year 
+    } //This will be the raw input for the methods that follow
 }
-    getIntDate() {
+    getIntDate() { //This should display the date as a string of integers with slashes between
         return `${this.day}/${this.month}/${this.year}'
     }
     
+    getDayMonth() {
+
+    }
+
 
 
 }
