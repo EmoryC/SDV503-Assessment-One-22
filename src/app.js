46166c6665d476d6a968c9b47f5aa89e439ddc83
EmoryC <emory-crane@live.nmit.ac.nz>
2022-05-01 20:36:05 +1200

    methods and parameters

--- src/app.js
+++ src/app.js
@@ -9,34 +9,53 @@ const dayspermonth = [0,31,28,31,30,31,30,31,31,30,31,30,31]
 //Creating a class _Date with which I will construct all functions (methods)
 class _Date{ 
     constructor(day,month,year){
-      if(year>2030){ //Setting an arbirary limit to the years to try it
+//Setting parameters for calander days
+        if(day>31){
+          console.log("Invalid Input")
+      } 
+      
+      else if(day<1){
+            console.log("Invalid Input")
+      } else {this.day = day}
+//Setting parameters for calander months      
+      if(month>12){
+            console.log("Invalid Input")
+      } else if(month<1){
+            console.log("Invalid Input")
+      } else {this.month = month}
+//Setting an arbitrary limit to the years to try it
+      if(year>2030){ 
         console.log("Invalid Input: Maximum year 2030")
     } else if(year<2000){
         console.log("Invalid Input: Minumim year 2000")
-    } else {
-        this.day = day
-        this.month = month
-        this.year = year 
-    } //This will be the raw input for the methods that follow
-}//This should display the date as a string of integers with slashes between
+    } else {this.year = year
+   } 
+}
+//This will be the raw input for the methods that follow   
+
+ //This should display the date as a string of integers with slashes between
     getIntDate() { 
         return `Date: ${this.day}/${this.month}/${this.year}`; //forgot the second backtick here and almost broke everything
     }
- //This should display the date as a string starting with integer day, 
+ //This should display the date starting with integer day, string month, then integer year
     getDayMonth() {
         return `Date: ${this.day} ${_Months[this.month]} ${this.year}`;
     }
+//This should display the date starting with string month, integer day, integer year
     getMonthDay() {
         return `Date: ${_Months[this.month]} ${this.day}, ${this.year}`
     }
-    
-    
-
-
-    
+//I could not get these to generate output but I believe individually they would operate fine
 
 
 
 }
+
+
+    
+
+
+
+
 const today = new _Date()
 console.log(today)
